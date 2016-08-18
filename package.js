83006349c97b1b796c358a032f39f82766f6c152
Keith Morgan <jmorgan@us.ibm.com>
2016-08-18 11:40:09 -0400

    update package

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('ibmcloud:bluemix@1.0.4', ['client', 'server']);
+  api.use('keithmorganibm:ibmid@1.0.0', ['client', 'server']);
 
   api.addFiles('ibmid_login_button.css', 'client');
   api.addFiles("ibm.js");
